112b6e413d133f927007049b5ec8ce696384bd08
Ben Thompson <t.ben.thompson@gmail.com>
2018-04-14 13:52:56 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,14 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# Commit and push the website source project 
+git status
+
+read -p "Are you you want to commit? (y/N)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git commit -a -m "update" 
+    git push
+fi
